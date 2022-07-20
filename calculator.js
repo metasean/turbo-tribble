eb41e5f79cf6652badda4988a316d2cba6310051
metasean <metasean@users.noreply.github.com>
2022-07-20 01:26:52 +0000

    saving example

---
+++ calculator.js
@@ -0,0 +1,34 @@
+// const a = 42;
+
+// function doSomething () {
+//     const a = 4;
+// }
+
+// doSomething();
+
+
+
+// function add (a, b) {
+//     return a + b;
+// }
+
+// const subtract = (a, b) => {
+//     return a - b;
+// }
+
+// const calculate = (operation, a, b) => {
+//     return operation(a, b);
+// }
+
+
+const objA = { 
+    a: 4
+ }
+
+console.log(objA.a)
+console.log(JSON.stringify({objA}))
+
+objA.a = 42;
+
+
+// console.log(calculate(add, 40, 2))
---
+++ script.js
@@ -0,0 +1,32 @@
+// function generateGreeter (greeting) {
+//     return  (name) => {
+//         console.log(greeting + ', ' + name + '!');
+//         console.log(`${greeting}, ${name}!`);
+//     }
+// }
+// const morningGreeting = generateGreeter('Hello');
+
+// morningGreeting('Ken');
+// morningGreeting('Brian');
+
+// say('Hello')('World');
+
+// function add (a, b) {
+//     return a + b;
+// }
+
+// const subtract = (a, b) => {
+//     return a - b;
+// }
+
+// const calculate = (operation, a, b) => {
+//     return operation(a, b);
+// }
+
+// console.log(calculate(add, 40, 2));
+// console.log(calculate(subtract, 40, 2));
+
+// const ten = 10;
+// const two = 2;
+
+// console.log(subtract(10, 2))
